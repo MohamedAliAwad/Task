9c7975141a618b1c02444ddf14180bdac14e0161
MohamedAliAwad <mohamed.ali.awad2018@gmail.com>
2021-10-08 02:47:23 +0200

    Improved Clac
    add do
    while

--- Calc.dart
+++ Calc.dart
@@ -2,34 +2,37 @@ import 'dart:io';
 
 void main() {
   print("Hello And Welcome!!");
-  print("Please enter number 1 !!");
-  String? n1 = stdin.readLineSync();
-  int number1 = int.parse(n1!);
-  // String? n2 = stdin.readLineSync();
-  // int number2 = int.parse(n1!);
-  if (number1 == 0) {
+  int? number1;
+  int? number2;
+  String? Agreement;
+  do {
+    print("Please enter number 1 !!");
+    number1 = int.parse(stdin.readLineSync()!);
+    if (number1 == 0) {
+      break;
+    }
+    print("Please the agreement !!");
+    Agreement = stdin.readLineSync();
+    print("Please enter number 2 !!");
+    number2 = int.parse(stdin.readLineSync()!);
+    Clac calc1 = Clac();
+
+    switch (Agreement) {
+      case "+":
+        print("Result : " + calc1.sum(number1, number2).toString());
+        break;
+      case "-":
+        print("Result : " + calc1.sub(number1, number2).toString());
+        break;
+      case "*":
+        print("Result : " + calc1.multi(number1, number2).toString());
+        break;
+      case "/":
+        print("Result : " + calc1.div(number1, number2).toString());
+    }
+  } while (number1 > 0);
+  {
     print("Applaction Closed");
-  } else
-    print("Please enter agreement !!");
-  String? Agreement = stdin.readLineSync();
-  print("Please enter number 2 !!");
-  String? n2 = stdin.readLineSync();
-  int number2 = int.parse(n2!);
-
-  Clac calc1 = Clac();
-
-  switch (Agreement) {
-    case "+":
-      print("Result : " + calc1.sum(number1, number2).toString());
-      break;
-    case "-":
-      print("Result : " + calc1.sub(number1, number2).toString());
-      break;
-    case "*":
-      print("Result : " + calc1.multi(number1, number2).toString());
-      break;
-    case "/":
-      print("Result : " + calc1.div(number1, number2).toString());
   }
 }
 
@@ -47,9 +50,6 @@ class Clac {
   }
 
   div(int num1, num2) {
-    if (num1 == 0) {
-      print("Can not divid by ZERO!!!");
-    } else
-      return num1 / num2;
+    return num1 / num2;
   }
 }
